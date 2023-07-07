251bd495f23a49bd6beab30311680196ea8347dd
Stefan <stefansandorovssb@gmail.com>
2023-07-07 14:48:16 +0200

    componentes de la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TodosModule } from './todos/todos.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TodosModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
