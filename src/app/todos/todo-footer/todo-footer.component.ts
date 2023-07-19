03d62e12e8d24ea0f050c18c3ec6ff8c22397b73
Stefan <stefansandorovssb@gmail.com>
2023-07-19 16:11:18 +0200

    cambiar entre los distintos valores del filtro en consola

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppState } from '../../app.reducer';
+import { Store } from '@ngrx/store';
+import * as actions from '../../filter/filter.actions';
 
 @Component({
   selector: 'app-todo-footer',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./todo-footer.component.css']
 })
 export class TodoFooterComponent {
+  actualFilter: actions.validFilters = 'all';
+  filters: actions.validFilters[] = ['all', 'completed', 'pending'];
 
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.store.select('filter').subscribe(filter => {
+      this.actualFilter = filter;
+    })
+  }
+
+  changeFilter(filter: actions.validFilters) {
+    this.store.dispatch(actions.setFilter({filter: filter}));
+  }
 }
