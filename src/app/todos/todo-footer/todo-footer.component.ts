df8a20b81d78956e9f76414cc5ec913567835d67
Stefan <stefansandorovssb@gmail.com>
2023-07-19 16:16:50 +0200

    conteo de tareas pendientes

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AppState } from '../../app.reducer';
 import { Store } from '@ngrx/store';
 import * as actions from '../../filter/filter.actions';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-todo-footer',
@@ -11,12 +12,18 @@ import * as actions from '../../filter/filter.actions';
 export class TodoFooterComponent {
   actualFilter: actions.validFilters = 'all';
   filters: actions.validFilters[] = ['all', 'completed', 'pending'];
+  pending: number = 0;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
-    this.store.select('filter').subscribe(filter => {
-      this.actualFilter = filter;
+    //* En caso de que sólo necesitasemos emplear uno de los estados
+    // this.store.select('filter').subscribe(filter => {
+    //   this.actualFilter = filter;
+    // })
+    this.store.subscribe(state => {
+      this.actualFilter = state.filter;
+      this.pending = state.todos.filter(todo => !todo.completed).length;
     })
   }
 
